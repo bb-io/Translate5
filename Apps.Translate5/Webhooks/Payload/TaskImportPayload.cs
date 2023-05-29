0eae374cb7185ea2f08cbc73e36ae80ad5a1bdc6
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-05-29 15:35:28 +0300

    Import finished callback added

---
+++ Apps.Translate5/Webhooks/Payload/TaskImportPayload.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Translate5.Webhooks.Payload
+{
+    public class TaskImportPayload
+    {
+        public string Id { get; set; }
+        public string EntityVersion { get; set; }
+        public string Modified { get; set; }
+        public string TaskGuid { get; set; }
+        public string TaskNr { get; set; }
+        public string ForeignId { get; set; }
+        public string TaskName { get; set; }
+        public string ForeignName { get; set; }
+        public string SourceLang { get; set; }
+        public string TargetLang { get; set; }
+        public string RelaisLang { get; set; }
+        public string State { get; set; }
+        public string Workflow { get; set; }
+        public string WorkflowStep { get; set; }
+        public string WorkflowStepName { get; set; }
+        public string PmGuid { get; set; }
+        public string PmName { get; set; }
+        public string WordCount { get; set; }
+        public string UserCount { get; set; }
+        public string ReferenceFiles { get; set; }
+        public string Terminologie { get; set; }
+        public string Orderdate { get; set; }
+        public string EnableSourceEditing { get; set; }
+        public string Edit100PercentMatch { get; set; }
+        public string LockLocked { get; set; }
+        public string EmptyTargets { get; set; }
+        public string ImportAppVersion { get; set; }
+        public string CustomerId { get; set; }
+        public string UsageMode { get; set; }
+        public string SegmentCount { get; set; }
+        public string SegmentFinishCount { get; set; }
+        public string TaskType { get; set; }
+        public string ProjectId { get; set; }
+        public string DiffExportUsable { get; set; }
+        public string Reimportable { get; set; }
+        public string Description { get; set; }
+        public string Created { get; set; }
+    }
+}
---
+++ Apps.Translate5/Webhooks/WebhookList.cs
@@ -0,0 +1,28 @@
+﻿using Apps.Translate5.Dtos;
+using Apps.Translate5.Webhooks.Payload;
+using Blackbird.Applications.Sdk.Common.Webhooks;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace Apps.Translate5.Webhooks
+{
+    [WebhookList]
+    public class WebhookList 
+    {
+
+        [Webhook("On task import finished", Description = "On task import finished")]
+        public async Task<WebhookResponse<TaskImportPayload>> TaskImportFinished(WebhookRequest webhookRequest)
+        {
+            var data = JsonConvert.DeserializeObject<TaskImportPayload>(webhookRequest.Body.ToString());
+            if(data is null)
+            {
+                throw new InvalidCastException(nameof(webhookRequest.Body));
+            }
+            return new WebhookResponse<TaskImportPayload>
+            {
+                HttpResponseMessage = null,
+                Result = data
+            };
+        }    
+    }
+}
