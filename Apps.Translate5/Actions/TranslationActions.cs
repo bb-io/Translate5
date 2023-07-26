f0e6138397dd62843b26419ff87d7d1095460557
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-07-26 20:13:50 +0300

    Instant text translation

---
+++ Apps.Translate5/Actions/TranslationActions.cs
@@ -0,0 +1,44 @@
+﻿using Apps.Translate5.Dtos;
+using Apps.Translate5.Models.Tasks.Requests;
+using Apps.Translate5.Models;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Actions;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apps.Translate5.Models.Translations.Requests;
+using Newtonsoft.Json.Linq;
+
+namespace Apps.Translate5.Actions
+{
+    [ActionList]
+    public class TranslationActions
+    {
+        [Action("Translate text with translate5 language resources", Description = "Translate text with translate5 language resources")]
+        public TranslationTextDto TranslateTextInstantly(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders,
+           [ActionParameter] TranslateTextInstantlyRequest input)
+        {
+            var tr5Client = new Translate5Client(authenticationCredentialsProviders);
+            var request = new Translate5Request($"/editor/instanttranslateapi/translate",
+                Method.Post, authenticationCredentialsProviders);
+            request.AlwaysMultipartFormData = true;
+
+            request.AddParameter("source", input.SourceLanguage);
+            request.AddParameter("target", input.TargetLanguage);
+            request.AddParameter("text", input.Text);
+
+            var intantTranslateResponse = JObject.Parse(tr5Client.Execute(request).Content);
+            var languageResourceExists = intantTranslateResponse.First.First.ToObject<JObject>().ContainsKey(input.LanguageResource);
+            if (!languageResourceExists)
+            {
+                throw new ArgumentException($"\"{input.LanguageResource}\" language resource is not configured");
+            }
+            var translations = intantTranslateResponse.First.First[input.LanguageResource].First.First.ToArray();
+            return translations.First().ToObject<TranslationTextDto>();
+        }
+    }
+}
---
+++ Apps.Translate5/Models/Translations/Requests/TranslateTextInstantlyRequest.cs
@@ -0,0 +1,23 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Translate5.Models.Translations.Requests
+{
+    public class TranslateTextInstantlyRequest
+    {
+        [Display("Source language")]
+        public string SourceLanguage { get; set; }
+
+        [Display("Target language")]
+        public string TargetLanguage { get; set; }
+
+        public string Text { get; set; }
+
+        [Display("Language resource (OpenTM2, DeepL etc.)")]
+        public string LanguageResource { get; set; }
+    }
+}
