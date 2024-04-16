f7d38954ce6a35071384a8c53f0c337be4167eba
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-16 18:49:08 +0300

    Categories (#7)
    * added categories
    
    * Update Apps.Translate5.csproj

--- Apps.Translate5/Translate5Application.cs
+++ Apps.Translate5/Translate5Application.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Translate5;
 
-public class Translate5Application : IApplication
+public class Translate5Application : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.CatAndTms];
+        set { }
+    }
+    
     public string Name
     {
         get => "Translate5";
