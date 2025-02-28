82e1fec05734c14faf0785d66341371c51b90002
RiabushenkoA <ariabushenko@blackbird.io>
2025-02-28 18:38:04 +0200

    Added error handling and Unit tests

---
+++ Tests.Translate5/TaskTests.cs
@@ -0,0 +1,22 @@
+﻿using Apps.Translate5.Actions;
+using Apps.Translate5.Models.Request.Tasks;
+using Translate5Tests.Base;
+
+namespace Tests.Translate5
+{
+    [TestClass]
+    public class TaskTests : TestBase
+    {
+        [TestMethod]
+        public async Task ExportTranslatedFileReturnsValues()
+        {
+            var action = new TaskActions(InvocationContext,FileManager);
+            var taskRequest = new TaskRequest { TaskId = "99443" };
+
+            var response = await action.ExportTaskFile(taskRequest);
+
+            Assert.IsNotNull(response);
+        }
+
+    }
+}
